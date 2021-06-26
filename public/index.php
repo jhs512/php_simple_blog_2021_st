2033d2b71758d09b2e9ff4155a380349877f959d
jhs512 <jangka512@gmail.com>
2021-06-26 17:35:16 +0900

    하단바 구현

--- public/index.php
+++ public/index.php
@@ -23,39 +23,59 @@
 </head>
 
 <body>
-    <header class="top-bar bg-gray-500 text-white h-10">
-        <div class="container mx-auto h-full flex">
-            <a href="/" class="top-bar__logo flex items-center px-4">
-                <span><i class="far fa-lemon"></i></span>
-                <span class="font-bold">&nbsp;LEMON IT</span>
-            </a>
+    <div class="site-wrap min-h-screen flex flex-col">
+        <header class="top-bar bg-gray-500 text-white h-10 flex-shrink-0">
+            <div class="container mx-auto h-full flex">
+                <a href="/" class="logo flex items-center px-4">
+                    <span><i class="far fa-lemon"></i></span>
+                    <span class="font-bold">&nbsp;LEMON IT</span>
+                </a>
 
-            <div class="flex-grow"></div>
+                <div class="flex-grow"></div>
 
-            <nav class="top-bar__menu-box-1">
-                <ul class="flex h-full">
-                    <li>
-                        <a href="/" class="h-full flex items-center px-4 hover:underline">
-                            <span><i class="fas fa-home"></i></span>
-                            <span class="font-bold hidden sm:block">&nbsp;HOME</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/jhs512" target="_blank" class="h-full flex items-center px-4 hover:underline">
-                            <span><i class="fab fa-github"></i></span>
-                            <span class="font-bold hidden sm:block">&nbsp;GITHUB</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.youtube.com/channel/UCiw6ewVgQkLMmgWjQp1-gKQ" target="_blank" class="h-full flex items-center px-4 hover:underline">
-                            <i class="fab fa-youtube"></i>
-                            <span class="font-bold hidden sm:block">&nbsp;YOUTUBE</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+                <nav class="top-bar__menu-box-1">
+                    <ul class="flex h-full">
+                        <li>
+                            <a href="/" class="h-full flex items-center px-4 hover:underline">
+                                <span><i class="fas fa-home"></i></span>
+                                <span class="font-bold hidden sm:block">&nbsp;HOME</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/jhs512" target="_blank"
+                                class="h-full flex items-center px-4 hover:underline">
+                                <span><i class="fab fa-github"></i></span>
+                                <span class="font-bold hidden sm:block">&nbsp;GITHUB</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCiw6ewVgQkLMmgWjQp1-gKQ" target="_blank"
+                                class="h-full flex items-center px-4 hover:underline">
+                                <i class="fab fa-youtube"></i>
+                                <span class="font-bold hidden sm:block">&nbsp;YOUTUBE</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <main class="flex-grow">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas laboriosam ipsam in quaerat nemo, iure ducimus eum suscipit soluta esse laudantium itaque consequuntur accusamus alias eos nobis sed quas?
+        </main>
+
+        <footer class="bottom-bar bg-gray-500 text-white h-10 flex-shrink-0">
+            <div class="container mx-auto h-full flex justify-center">
+                <a href="/" class="logo flex items-center">
+                    <span><i class="far fa-lemon"></i></span>
+                    <span class="font-bold">&nbsp;LEMON IT</span>
+                </a>
+                <div class="flex items-center">
+                    &nbsp;IS GOOD!
+                </div>
+            </div>
+        </footer>
+    </div>
 </body>
 
 </html>
